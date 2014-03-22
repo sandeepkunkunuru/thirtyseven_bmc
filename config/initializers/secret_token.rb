55a91b8571ecc19eae291ed125feca19d4814cde
Sandeep Kunkunuru <sandeep.kunkunuru@gmail.com>
2014-03-21 23:49:17 -0400

    Initializing the application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThirtysevenBmc::Application.config.secret_key_base = 'b064407a5441b3fecd59e323dc2123d818d1525e38671946c17992f394d869b31caf7aa7e4ac51cbbb310442cb80c9daf824e8eb0da15140a5d6265335fa1abd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThirtysevenBmc::Application.config.session_store :cookie_store, key: '_thirtyseven_bmc_session'
